85e8958728cfa9639d7363e0eb86c5553fd9c168
wjjung <wjjung@wemakeprice.com>
2018-06-21 15:46:48 +0900

    ... fork를 안 해서 pullrequest가 안... 생겨...

--- public/src/js/index.js
+++ public/src/js/index.js
@@ -1,6 +1,8 @@
-
 //jquery
 window.jQuery = window.$ = require('jquery');
+
+// sweetalert2
+window.Swal = require('sweetalert2');
 
 //bootstrap
 require('bootstrap');
@@ -12,7 +14,10 @@ var html = Prism.highlight(code, Prism.languages.javascript, 'javascript');
 
 //all
 const allFiles = (ctx => {
-    let keys = ctx.keys();
-    let values = keys.map(ctx);
-    return keys.reduce((o, k, i) => { o[k] = values[i]; return o; }, {});
+  let keys = ctx.keys();
+  let values = keys.map(ctx);
+  return keys.reduce((o, k, i) => {
+    o[k] = values[i];
+    return o;
+  }, {});
 })(require.context('../', true, /.*/));
